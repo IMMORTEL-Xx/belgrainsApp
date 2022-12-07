65487344b6c5dd3eee17ec121f1ab680ca1145cd
Andres <delgado.andres379@gmail.com>
2022-12-07 15:07:50 +0100

    Ajout des boutons déroulants

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -5,8 +5,34 @@ import { Component } from '@angular/core';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
 
+  liste1 = false;
+  liste2 = false;
+  liste3 = false;
+  
   constructor() {}
+
+  deroulerListe1(){
+    this.liste1 = !this.liste1;
+    this.liste2 = false;
+    this.liste3 = false;
+    console.log(this.liste1);
+  }
+
+  deroulerListe2(){
+    this.liste2 = !this.liste2;
+    this.liste1 = false;
+    this.liste3 = false;
+    console.log(this.liste1);
+  }
+
+  deroulerListe3(){
+    this.liste3 = !this.liste3;
+    this.liste1 = false;
+    this.liste2 = false;
+    console.log(this.liste1);
+  }
 
 }
