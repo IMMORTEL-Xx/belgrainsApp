bde8dbb721a6bae01c2b22da7b0c416c3f06bddf
Andres <delgado.andres379@gmail.com>
2022-12-07 17:08:56 +0100

    ajout logo belgrains

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -18,21 +18,18 @@ export class HomePage {
     this.liste1 = !this.liste1;
     this.liste2 = false;
     this.liste3 = false;
-    console.log(this.liste1);
   }
 
   deroulerListe2(){
     this.liste2 = !this.liste2;
     this.liste1 = false;
     this.liste3 = false;
-    console.log(this.liste1);
   }
 
   deroulerListe3(){
     this.liste3 = !this.liste3;
     this.liste1 = false;
     this.liste2 = false;
-    console.log(this.liste1);
   }
 
 }
